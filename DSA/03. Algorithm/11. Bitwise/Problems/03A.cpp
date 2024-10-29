58fecfc4a8366a32b28d1a1c826aff5995c30748
Rishikesh <rishikeshpanditrp@gmail.com>
2024-10-29 15:04:30 +0530

    new

---
+++ DSA/03. Algorithm/11. Bitwise/Problems/03A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void addTwoBits(string s1, string s2){
+    bool carry = false;
+    int i = s1.length(), j = s2.length();
+    string res = "";
+    
+    while(i and j){
+        if(carry){
+            if(s1[i-1] == '1')
+        }
+    }
+}
+
+int main(){
+    stirng s1, s2;
+    cin >> s1 >> s2;
+
+    cout << addTwoBits(s1, s2);
+
+    return 0;
+}
